144d339334e7acbca34a3a78331f2b94800bf648
Sapana Kale <44019343+sapanaKale@users.noreply.github.com>
2019-04-15 19:50:37 +0530

    Updates

---
+++ precache-manifest.e4450d18fe89ef72018b56d6a6e8919f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d4cc52b58bd19065a8a",
+    "url": "/solitaire/static/js/runtime~main.1eee08ec.js"
+  },
+  {
+    "revision": "54ed6b0ca03a4cb1f156",
+    "url": "/solitaire/static/js/main.d3f83180.chunk.js"
+  },
+  {
+    "revision": "1852c1a922611693b648",
+    "url": "/solitaire/static/js/2.1e33e6c5.chunk.js"
+  },
+  {
+    "revision": "54ed6b0ca03a4cb1f156",
+    "url": "/solitaire/static/css/main.50cf16d6.chunk.css"
+  },
+  {
+    "revision": "94c32de563f088dbb57ea913e6e3838f",
+    "url": "/solitaire/index.html"
+  }
+];
